a32cba6ab1ce94769c62a05b45657c2daf034fc5
Thomas Blaisot <3236741+tblaisot@users.noreply.github.com>
2025-11-19 15:23:37 +0100

    feat: remove the syntaxe highlighter at asciidoctor build time because it is deprecated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import * as path from "path";
 import asciidoctorFactory from "@asciidoctor/core";
-import * as highlightJsExt from "asciidoctor-highlight.js";
 import * as url from 'url';
 import * as attachedColistTreeprocessorExt from "./extensions/attached_colist_treeprocessor.js";
 
@@ -10,13 +9,11 @@ export const asciidoctor = asciidoctorFactory();
 
 // Register the extension into custom registry.
 const registry = asciidoctor.Extensions.create();
-highlightJsExt.register(registry);
 attachedColistTreeprocessorExt.register(registry);
 
 export const REGISTRY = registry;
 export const BASE_OPTIONS = {
     extension_registry: registry,
-    attributes: {'source-highlighter': 'highlightjs-ext'},
     catalog_assets: true,
     safe: 'unsafe',
     backend: 'html',
