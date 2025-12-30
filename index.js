bf4434d7930d3e54efb95cc56a4281ef3fb1fc5d
Thomas Blaisot <3236741+tblaisot@users.noreply.github.com>
2025-12-30 10:16:41 +0100

    feat: export registry as a function instead of a const
    This is to allow usage in other plugins preventing a bug where if the same registry is reused for multiple docs it don't work
    
    BREAKING CHANGE

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 import * as path from "path";
-import asciidoctorFactory from "@asciidoctor/core";
 import * as url from 'url';
 import * as attachedColistTreeprocessorExt from "./extensions/attached_colist_treeprocessor.js";
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-export const asciidoctor = asciidoctorFactory();
+export function registerExtensions(registry){
+    attachedColistTreeprocessorExt.register(registry);
+}
 
-// Register the extension into custom registry.
-const registry = asciidoctor.Extensions.create();
-attachedColistTreeprocessorExt.register(registry);
-
-export const REGISTRY = registry;
 export const BASE_OPTIONS = {
-    extension_registry: registry,
     catalog_assets: true,
     safe: 'unsafe',
     backend: 'html',
